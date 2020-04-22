d0ea3c688b71919f0b2c0594b0adcd3b3960af47
ffuchs-tgm <ffuchs@student.tgm.ac.at>
2020-04-22 17:01:29 +0200

    Color selection successful and detection if field is clicked implemented

--- mastermind/main.py
+++ mastermind/main.py
@@ -18,6 +18,7 @@ MAGENTA = (255, 0, 255)
 GREY = (128, 128, 128)
 DARK_GREY = (96, 96, 96)
 SILVER = (192, 192, 192)
+colors = {'RED': (255, 0, 0), 'GREEN': (0, 255, 0), 'BLUE': (0, 0, 255), 'CYAN': (0, 255, 255), 'MAGENTA': (255, 0, 255), 'YELLOW': (255, 255, 0)}
 
 # Open a new window
 size = (280, 620)
@@ -26,14 +27,69 @@ screenWidth = screen.get_rect().width
 screenHeight = screen.get_rect().height
 pygame.display.set_caption("TGMastermind!")
 numbers = []
-guesses = [[]]
-hints = [[]]
-colors = []
+guesses = []
+hints = []
+selectableColors = {}
 confirm = 0
+selected_color = ""
 temp = []
 
 myfont = pygame.font.SysFont('Raleway', 50)
 textsurface = myfont.render('MASTERMIND', True, WHITE)
+
+screen.fill(DARK_GREY)
+
+# MASTERMIND text
+screen.blit(textsurface, (screenWidth / 2 - (textsurface.get_rect().width / 2), 10))
+
+# 4 Mysterious Dots
+for i in range(0, 4):
+    pygame.draw.circle(screen, SILVER, ((int(screenWidth / 2) - 70) + 50 * i, 70), 20, 0)
+# Background rect
+pygame.draw.rect(screen, GREY, [0, 100, screenWidth, screenHeight - 100], 0)
+
+# 10 rows for payling field
+for i in range(0, 11):
+    # Number rects
+    numbers.append(pygame.draw.rect(screen, WHITE, [10, 110 + i * 40, 30, 30], 0))
+    # 4 empty color selections for the guessing player
+    for j in range(0, 4):
+        temp.append(pygame.draw.circle(screen, SILVER, (70 + 50 * j, 125 + i * 40), 15, 0))
+        #print(temp)
+    # Border for the hints
+    guesses.append(temp.copy())
+    #print(guesses)
+    temp.clear()
+    pygame.draw.rect(screen, WHITE, [screenWidth - 35, 110 + i * 40, 30, 30], 1)
+    # 4 hints from the mastermind to the guesser
+    for j in range(0, 4):
+        if j < 2:
+            temp.append(pygame.draw.circle(screen, SILVER, (screenWidth - 27 + 14 * j, 118 + i * 40), 5, 0))
+        else:
+            temp.append(pygame.draw.circle(screen, SILVER, (screenWidth - 27 + 14 * (j - 2), 132 + i * 40), 5, 0))
+    hints.append(temp.copy())
+    temp.clear()
+# 6 colors that can be selected
+selectableColors.update(
+    {'RED': pygame.draw.circle(screen, RED, (int(screenWidth / 6) * 1 - 18, screenHeight - 55), 15, 0)})
+selectableColors.update(
+    {'GREEN': pygame.draw.circle(screen, GREEN, (int(screenWidth / 6) * 2 - 18, screenHeight - 55), 15, 0)})
+selectableColors.update(
+    {'BLUE': pygame.draw.circle(screen, BLUE, (int(screenWidth / 6) * 3 - 18, screenHeight - 55), 15, 0)})
+selectableColors.update(
+    {'CYAN': pygame.draw.circle(screen, CYAN, (int(screenWidth / 6) * 4 - 18, screenHeight - 55), 15, 0)})
+selectableColors.update(
+    {'MAGENTA': pygame.draw.circle(screen, MAGENTA, (int(screenWidth / 6) * 5 - 18, screenHeight - 55), 15, 0)})
+selectableColors.update(
+    {'YELLOW': pygame.draw.circle(screen, YELLOW, (int(screenWidth / 6) * 6 - 18, screenHeight - 55), 15, 0)})
+# confirm button
+confirm = pygame.draw.rect(screen, WHITE, [10, screenHeight - 35, int(screenWidth / 2) - 15, 30], 0)
+# solve button
+solve = pygame.draw.rect(screen, WHITE,
+                         [int(screenWidth / 2) + 5, screenHeight - 35, int(screenWidth / 2) - 10, 30], 0)
+
+
+
 
 # The loop will carry on until the user exit the game (e.g. clicks the close button).
 carryOn = True
@@ -47,52 +103,27 @@ while carryOn:
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             carryOn = False  # Flag that we are done so we exit this loop
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+
+            # Color selection
+            for c in colors:
+                for sc in selectableColors:
+                    if selectableColors[c].collidepoint(pos):
+                        selected_color = c
+            print(selected_color)
+            # check if a guessing field has been selected
+            #print(guesses)
+            for row in guesses:
+                print(row)
+                for color in row:
+                    print(color.collidepoint(pos))
 
         # --- Game logic should go here
 
         # --- Drawing code should go here
         # First, clear the screen to white.
-    screen.fill(DARK_GREY)
 
-    # MASTERMIND text
-    screen.blit(textsurface, (screenWidth / 2 - (textsurface.get_rect().width / 2), 10))
-
-    # 4 Mysterious Dots
-    for i in range(0, 4):
-        pygame.draw.circle(screen, SILVER, ((int(screenWidth / 2) - 70) + 50 * i, 70), 20, 0)
-    # Background rect
-    pygame.draw.rect(screen, GREY, [0, 100, screenWidth, screenHeight - 100], 0)
-
-    # 10 rows for payling field
-    for i in range(0, 11):
-        # Number rects
-        numbers.append(pygame.draw.rect(screen, WHITE, [10, 110 + i * 40, 30, 30], 0))
-        # 4 empty color selections for the guessing player
-        for j in range(0, 4):
-            temp.append(pygame.draw.circle(screen, SILVER, (70 + 50 * j, 125 + i * 40), 15, 0))
-        # Border for the hints
-        guesses.append(temp)
-        temp.clear()
-        pygame.draw.rect(screen, WHITE, [screenWidth - 35, 110 + i * 40, 30, 30], 1)
-        # 4 hints from the mastermind to the guesser
-        for j in range(0, 4):
-            if j < 2:
-                temp.append(pygame.draw.circle(screen, SILVER, (screenWidth - 27 + 14 * j, 118 + i * 40), 5, 0))
-            else:
-                temp.append(pygame.draw.circle(screen, SILVER, (screenWidth - 27 + 14 * (j - 2), 132 + i * 40), 5, 0))
-        hints.append(temp)
-        temp.clear()
-    # 6 colors that can be selected
-    colors.append(pygame.draw.circle(screen, RED, (int(screenWidth / 6) * 1 - 18, screenHeight - 55), 15, 0))
-    colors.append(pygame.draw.circle(screen, BLUE, (int(screenWidth / 6) * 2 - 18, screenHeight - 55), 15, 0))
-    colors.append(pygame.draw.circle(screen, GREEN, (int(screenWidth / 6) * 3 - 18, screenHeight - 55), 15, 0))
-    colors.append(pygame.draw.circle(screen, YELLOW, (int(screenWidth / 6) * 4 - 18, screenHeight - 55), 15, 0))
-    colors.append(pygame.draw.circle(screen, CYAN, (int(screenWidth / 6) * 5 - 18, screenHeight - 55), 15, 0))
-    colors.append(pygame.draw.circle(screen, MAGENTA, (int(screenWidth / 6) * 6 - 18, screenHeight - 55), 15, 0))
-    # confirm button
-    confirm = pygame.draw.rect(screen, WHITE, [10, screenHeight - 35, int(screenWidth/2) - 15, 30], 0)
-    # solve button
-    solve = pygame.draw.rect(screen, WHITE, [int(screenWidth/2) + 5, screenHeight - 35, int(screenWidth/2) - 10, 30], 0)
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
--- mastermind/mastermind.py
+++ mastermind/mastermind.py
@@ -3,7 +3,7 @@ import numpy as np
 class Mastermind:
     # This class represents a car. It derives from the "Sprite" class in Pygame.
     def __init__(self):
-        self.colors = ['RED', 'BLUE', 'GREEN', 'YELLOW', 'MAGENTA', 'CYAN']
+        self.colors = ['RED', 'GREEN', 'BLUE', 'CYAN', 'MAGENTA', 'YELLOW']
         self.tries = 0
         self.code = []
         for i in range(0, 4):
